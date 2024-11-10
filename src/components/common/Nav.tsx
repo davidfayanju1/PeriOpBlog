b2ca621f89cf07cb611070a60dca08388acd82bc
davidfayanju1 <davidfayanju01@gmail.com>
2024-11-10 06:36:18 +0100

    Starting parts

--- src/components/common/Nav.tsx
+++ src/components/common/Nav.tsx
@@ -1,23 +1,61 @@
 import { Link } from "react-router-dom";
 
 const Nav = () => {
+  const links = [
+    {
+      name: "Books",
+      route: "/books",
+    },
+    {
+      name: "Articles",
+      route: "/articles",
+    },
+    {
+      name: "Book Details",
+      route: "/book-details",
+    },
+
+    {
+      name: "Blogs",
+      route: "/blogs",
+    },
+
+    {
+      name: "Event",
+      route: "/events",
+    },
+  ];
+
   return (
     <div>
-      <nav className="item_container fixed top-0 left-0 flex items-center justify-between w-full min-h-[6rem] bg-gray-500 md:px-[2rem] px-[1rem]">
+      <nav className="item_container fixed top-0 left-0 flex items-center justify-between w-full min-h-[6rem] bg-white md:px-[2rem] px-[1rem]">
         <div className="text_title">
-          <h1 className="font-bold md:text-[2rem] font-mono">POB.</h1>
+          <h1 className="font-bold md:text-[1.5rem] text-[1.2rem] font-mono">
+            POB.
+          </h1>
         </div>
 
-        <div className="links_section">
-          <ul className="">
-            <li className=" flex items-center justify-center gap-3">
-              <Link to={"/"}>Books</Link>
-              <Link to={"/article"}>Articles</Link>
-              <Link to={"/book-details"}>Book Details</Link>
-              <Link to={"/blods"}>Blogs</Link>
-              <Link to={"/events"}>Events</Link>
-            </li>
-          </ul>
+        <ul className="md:block hidden">
+          <li className=" flex items-center justify-center gap-4">
+            {links.map((item) => (
+              <Link to={item.route} className="font-semibold text-[1rem]">
+                {item.name}
+              </Link>
+            ))}
+          </li>
+        </ul>
+
+        <div className="icon_container flex items-center justify-center gap-5 h-full">
+          <button className="text-[1.1rem] text-black">
+            <i className="fa-solid fa-magnifying-glass"></i>
+          </button>
+          <button className="text-[1.1rem] text-black md:block hidden">
+            <i className="fa-solid fa-cart-shopping"></i>
+          </button>
+
+          <button className="text-black text-[1.1rem] md:hidden block">
+            <i className="fa-solid fa-bars"></i>
+          </button>
         </div>
       </nav>
     </div>
--- src/components/modules/home/HomeComp.tsx
+++ src/components/modules/home/HomeComp.tsx
@@ -1,7 +1,7 @@
 const HomeComp = () => {
   return (
     <div className="item-container min-h-screen">
-      <section className="section_container min-h-[10rem] bg-gray-200">
+      <section className="section_container min-h-[25rem] bg-gray-100">
         <div className="text-container"></div>
       </section>
     </div>
