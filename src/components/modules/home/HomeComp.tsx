d0c61c1b8b83a8f1d14c835f82e12a90db9b406a
davidfayanju1 <davidfayanju01@gmail.com>
2024-11-10 22:36:13 +0100

    Finishing touches for today

--- src/components/modules/home/HomeComp.tsx
+++ src/components/modules/home/HomeComp.tsx
@@ -2,33 +2,45 @@ import Button from "../../common/Button";
 
 const HomeComp = () => {
   return (
-    <div className="bg-white min-h-screen">
+    <div className="bg-gray-50 min-h-screen">
       <div className="item-container md:h-[35rem] h-[30rem] flex items-center justify-center gap-10 p-3 w-full">
-        <section className="section_container md:px-[3.5rem] px-[1rem] md:py-[3rem] py-[6rem] h-full bg-gray-100 rounded-[10px] md:w-[70%] w-full">
-          <div className="text-container">
-            <h1 className="font-bold poppins-bold md:w-[50%] w-full md:text-left text-center md:text-[3.1rem] text-[2rem]">
+        <section className="section_container flex items-center justify-between md:px-[3.5rem] px-[1rem] md:py-[3rem] py-[6rem] h-full bg-white rounded-[10px] md:w-[70%] w-full">
+          <div className="text-container h-full flex flex-col justify-center">
+            <h1 className="poppins-bold md:w-[70%] w-full md:text-left text-center md:text-[3vw] text-[2rem]">
               WELCOME TO PERIOPBLOG
             </h1>
-            <span className="block poppins-regular mt-1 text-gray-400 md:text-[1.4rem] md:text-left text-center text-[1rem]">
+            <span className="block poppins-regular mt-1 text-gray-400 md:text-left text-center text-[1rem] md:w-[60%] w-full">
               A house to millions of digital and non-digital books.
             </span>
 
-            <div className="input_container flex items-center px-7 justify-center border-solid mt-9 border-[2px] border-black rounded-full w-full h-[3rem]">
+            <div className="input_container flex items-center px-7 justify-center border-solid mt-9 border-[2px] border-black rounded-full md:w-[60%] w-full h-[3rem]">
               <input
                 type="text"
-                className="outline-none poppins-regular bg-transparent w-full text-[16px]"
+                className="outline-none poppins-regular placeholder:text-[14px] bg-transparent w-full text-[16px]"
                 placeholder="Write Title Here"
               />
 
               <Button
                 title="Search"
                 onPress={() => console.log("ok")}
-                buttonStyle="h-full bg-black poppings-regular text-white font-semibold w-full mr-[-2rem] rounded-full"
+                buttonStyle="h-full bg-black poppins-medium text-white font-semibold w-full mr-[-2rem] rounded-full"
               />
             </div>
           </div>
+
+          <div className="picture_container md:block hidden w-[40%] rounded-[10px] overflow-hidden">
+            <img
+              src="/images/useThis.jpg"
+              alt="hero-image"
+              className="h-[30rem] w-full"
+            />
+          </div>
         </section>
-        <section className="h-full bg-gray-100 w-[25%] md:block hidden"></section>
+        <section className="h-full bg-white w-[25%] md:block hidden rounded-[10px] overflow-hidden">
+          <div className="bubble rounded-full bg-gray-300 h-[8rem] w-[8rem] ml-[-3rem] mt-[-3rem]"></div>
+          <div className="bubble rounded-full bg-gray-300 h-[8rem] w-[8rem]"></div>
+          <div className="book_container"></div>
+        </section>
       </div>
     </div>
   );
