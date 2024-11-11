505c38077f689cea9c541780a6aa8d1ddc73064b
davidfayanju1 <davidfayanju01@gmail.com>
2024-11-11 08:52:12 +0100

    Completion of hero section

--- src/components/common/Nav.tsx
+++ src/components/common/Nav.tsx
@@ -31,7 +31,7 @@ const Nav = () => {
 
   return (
     <div>
-      <nav className="item_container fixed top-0 left-0 flex items-center justify-between w-full min-h-[6rem] bg-white md:px-[2rem] px-[1rem]">
+      <nav className="item_container z-30 fixed top-0 left-0 flex items-center justify-between w-full min-h-[6rem] bg-white md:px-[2rem] px-[1rem]">
         <div className="text_title">
           <h1 className="font-bold md:text-[1.5rem] text-[1.2rem] poppins-bold">
             POB.
--- src/components/modules/home/HomeComp.tsx
+++ src/components/modules/home/HomeComp.tsx
@@ -3,9 +3,40 @@ import Button from "../../common/Button";
 const HomeComp = () => {
   return (
     <div className="bg-gray-50 min-h-screen">
-      <div className="item-container md:h-[35rem] h-[30rem] flex items-center justify-center gap-10 p-3 w-full">
-        <section className="section_container flex items-center justify-between md:px-[3.5rem] px-[1rem] md:py-[3rem] py-[6rem] h-full bg-white rounded-[10px] md:w-[70%] w-full">
-          <div className="text-container h-full flex flex-col justify-center">
+      <div className="item-container md:h-[35rem] h-[69rem] flex items-center md:flex-row flex-col justify-center gap-10 p-3 w-full">
+        <section className="section_container flex items-center justify-between overflow-hidden md:px-[3.5rem] md:py-[3rem] h-full bg-white rounded-[10px] md:w-[70%] w-full">
+          <div className="mobile_image_container md:hidden relative block h-full w-full">
+            <div className="bg-[#000000]/25 z-10 h-full w-full absolute top-0 left-0 right-0 bottom-0"></div>
+            <img
+              src="/images/heroOfficial.jpg"
+              alt="hero-image"
+              className="h-full w-full object-cover relative"
+            />
+            <div className="text_container w-[90%] absolute z-20 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
+              <h1 className="poppins-bold text-white w-full text-center text-[1.9rem]">
+                WELCOME TO PERIOPBLOG
+              </h1>
+              <span className="block poppins-regular mt-1 text-white md:text-left text-center text-[1rem] w-full">
+                A house to millions of digital and non-digital books.
+              </span>
+
+              <div className="input_container flex items-center px-7 justify-center border-solid mt-9 border-[2px] border-white rounded-full md:w-[60%] w-full h-[3rem]">
+                <input
+                  type="text"
+                  className="outline-none text-white poppins-regular placeholder:text-[14px] bg-transparent w-full text-[16px]"
+                  placeholder="Write Title Here"
+                />
+
+                <Button
+                  title="Search"
+                  onPress={() => console.log("ok")}
+                  buttonStyle="!h-full bg-white poppins-medium text-black font-semibold w-full mr-[-2rem] rounded-full"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="desktop-container h-full md:flex flex-col justify-center hidden">
             <h1 className="poppins-bold md:w-[70%] w-full md:text-left text-center md:text-[3vw] text-[2rem]">
               WELCOME TO PERIOPBLOG
             </h1>
@@ -28,18 +59,44 @@ const HomeComp = () => {
             </div>
           </div>
 
-          <div className="picture_container md:block hidden w-[40%] rounded-[10px] overflow-hidden">
+          <div className="picture_container md:block hidden w-[45%] rounded-[10px] overflow-hidden">
             <img
               src="/images/heroOfficial.jpg"
               alt="hero-image"
-              className="h-[25rem] w-full"
+              className="h-[25rem] w-full object-cover"
             />
           </div>
         </section>
-        <section className="h-full bg-white w-[25%] md:block hidden rounded-[10px] overflow-hidden">
+        <section className="h-full relative bg-white md:w-[25%] w-full rounded-[10px] overflow-hidden">
           <div className="bubble rounded-full bg-gray-300 h-[8rem] w-[8rem] ml-[-3rem] mt-[-3rem]"></div>
-          <div className="bubble rounded-full bg-gray-300 h-[8rem] w-[8rem]"></div>
-          <div className="book_container"></div>
+          <div className="bubble absolute right-[5%] rounded-full bg-gray-300 h-[2.8rem] w-[2.8rem]"></div>
+          <div className="book_container">
+            <h1 className="poppins-semibold mb-3 text-center text-[1.8rem]">
+              NEW RELEASE
+            </h1>
+            <div className="book_container mb-4 h-[12rem] mx-auto">
+              <img
+                src="/images/shift.jpg"
+                alt="book/article-cover"
+                className="h-full mx-auto shadow-md"
+              />
+            </div>
+            <div className="texts">
+              <span className="title block poppins-semibold text-center text-[20px]">
+                The Shift
+              </span>
+              <span className="author block poppins-regular text-center text-gray-400 text-[16px]">
+                Theresa Brown
+              </span>
+
+              <Button
+                title="Preview"
+                buttonStyle="bg-black text-white poppins-medium mt-4 rounded-full h-[3rem] w-[50%] mx-auto"
+                onPress={() => console.log("Ok")}
+              />
+            </div>
+            <div className="bubble absolute bottom-[10%] left-[5%] rounded-full bg-gray-300 h-[2.8rem] w-[2.8rem]"></div>
+          </div>
         </section>
       </div>
     </div>
